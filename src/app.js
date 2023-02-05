d4576401949a970ba5dddf5898797c13eaeaba3a
Lisandro Palavecino <119263397+LisandroP57@users.noreply.github.com>
2023-02-05 13:00:07 -0300

    add all

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const path = require("path");
+
+app.use(express.static("public"));
+
+    /* Template engine */
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+    /* Routers */
+const indexRouter = require("./routes");
+
+    /* Routes Middlewares */
+app.use("/", indexRouter);
+
+app.listen(PORT, () => console.log(`Server listen in port ${PORT}\nhttp://localhost:${PORT}`));
---
+++ src/controllers/indexController.js
@@ -0,0 +1,12 @@
+const { carousel, products, categories } = require("../database")
+
+module.exports = {
+
+    index: (req, res) => {
+        res.render("index", {
+            carousel,
+            sliderTitle: "Productos en oferta",
+            sliderProducts: products
+        })
+    }
+}
